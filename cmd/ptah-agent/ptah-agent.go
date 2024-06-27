deb738d63bdedd826cc2fb6d7360c144759c3da5
Bohdan Shulha <b.shulha@pm.me>
2024-06-27 12:13:56 +0200

    feat: #9 add installation script

--- cmd/ptah-agent/ptah-agent.go
+++ cmd/ptah-agent/ptah-agent.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 	ptahAgent "github.com/ptah-sh/ptah-agent/internal/app/ptah-agent"
 	"log"
+	"os"
+	"strings"
 )
 
 var version string = "dev"
 
 func main() {
-	baseUrl := "http://localhost:8000/api/_nodes/v1"
-	ptahToken := "aSpD6hq28mIUbdfP0lh6HgqGXVNQRv4SLwNCTHLwFh"
+	baseUrl := os.Getenv("PTAH_BASE_URL")
+	if baseUrl == "" {
+		log.Println("PTAH_BASE_URL is not set, using http://localhost:8000")
+
+		baseUrl = "http://localhost:8000"
+	}
+
+	baseUrl = strings.Trim(baseUrl, "/")
+	baseUrl = fmt.Sprintf("%s/api/_nodes/v1", baseUrl)
+
+	ptahToken := os.Getenv("PTAH_TOKEN")
+	if ptahToken == "" {
+		log.Println("PTAH_TOKEN is not set, using localhost-ptah-token")
+
+		ptahToken = "localhost-ptah-token"
+	}
 
 	agent := ptahAgent.New(version, baseUrl, ptahToken)
 
